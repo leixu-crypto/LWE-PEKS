b534aaed33d61b98a82a40bd0529038647bcde63
Raycrypto <33500723+Raycrypto@users.noreply.github.com>
2019-07-03 14:57:55 +0800

    Update search.m

--- search.m
+++ search.m
@@ -8,12 +8,17 @@ else
 end
 end
 
+ 
 function st=tokgen(sk,w)
 [n,m]=size(sk);
+T=load(matrix_T.txt);
+M=load(matrix_M.txt);
 for j=1:k
+    T{j}=T(:,2j-1:2j);
     T1=tau(j)*T{j}+T1;
 end
 for j=1:k
+    M{j}=M(:,2*j*(m+2)-(m+2)+1:2*j*(m+2));
     M1=w(j)*M{j}+M1;
 end
 A_tau=[A,mod(T1,q)];
